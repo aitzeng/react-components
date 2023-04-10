fb8643f2c4e7c7991ed35b1d156cc5a78ef3f7a7
aitzeng <aitzeng@gmail.com>
2023-04-10 12:50:51 -0700

    Added dynamic list and onClick/onMouseOver features

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 // TODO
+const App = () => (
+  <div>
+    <p>My Grocery List</p>
+    <Items list={['apples', 'bananas', 'oranges', 'candy', 'cereal']}/>
+  </div>
+)
+
+const Items = (props) => {
+
+  // const onListItemClick = (event) => {
+  //   console.log('I got clicked');
+  // }
+
+  return (
+
+  <ul>
+    {props.list.map((item) => (
+      <TodoListItem fruit={item}/>
+    ))}
+  </ul>
+  );
+};
+
+const {useState} = React;
+
+const TodoListItem = (props) => {
+
+  const [isDone, setIsDone] = useState(false);
+  const [isBold, setBold] = useState(false);
+
+  const style = {
+    textDecoration: isDone  ? 'line-through' : 'none',
+    fontWeight: isBold ? 'Bold' : 'Normal'
+  };
+
+  return (
+  <li style={style} onClick={() => setIsDone(!isDone)} onMouseOver={() => setBold(!isBold)} >{props.fruit}</li>
+  )
+}
+
+// const Item2 = () => (
+//   <ul>
+//     <li>{props.items[1]}</li>
+//   </ul>
+// )
+
+ReactDOM.render(<App />, document.getElementById('app'))
